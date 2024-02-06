9f8b2bc13cfcc27794c676e864933e4f38374ccb
mostafa <mostafaalashry397@gmail.com>
2024-02-06 18:54:58 +0200

    Initial Commit

---
+++ Fish Book/Fish_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fish_BookApp.swift
+//  Fish Book
+//
+//  Created by mostafa on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fish_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
